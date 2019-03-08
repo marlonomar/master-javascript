f8549bdae286771a72d5ae85ec1ca706a8b9447a
Garcia <marlon.omar.g.garcia@accenture.com>
2019-03-08 13:55:18 -0300

    cambios en el reloj

--- eventos.js
+++ eventos.js
@@ -219,14 +219,19 @@ $(document).ready(function(){
         let hora = data.getHours();
         let minutos = data.getMinutes();
         let segundos = data.getSeconds();
+        let dia = data.getDate();
+        let mes = data.getMonth() +1;
+        let year = data.getFullYear();
 
         var reloj =`
             <div id="reloj_digital">
                 <h1>${hora} : ${minutos} :${segundos}</h1>
+                <h2>${dia} / ${mes} / ${year}</h2>
             </div>
         `;
         $("#cont").empty();
         $("#cont").append(reloj);
+        
             
 
        }
